a73dd7d333905917040ab8f98bf93b9c4a564737
Serafino Barwinski <110348589+BraucheeinPsychologe@users.noreply.github.com>
2025-12-05 11:41:46 +0100

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -1,9 +1,16 @@
-import fs from "fs/promises";
+import { promises as fs } from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const modulesPath = "./modules"; // Pfad zu deinen Modulen
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const modulesPath = path.join(__dirname, "..", "modules");
+
+let logging = process.argv.slice(2) || "default";
+
+const modules = await listModules()
 const modules_loaded = [];
-let logging = "debug"; // optional
 
 async function listModules() {
   const files = await fs.readdir(modulesPath);
@@ -11,19 +18,28 @@ async function listModules() {
     .filter(file => file.endsWith(".js"))
     .map(file => file.slice(0, -3));
   console.log("[LOG] Found", jsFiles.length, "Modules");
-  if (logging === "debug") console.log("[DEBUG] Found Modules: ", jsFiles);
+  if (logging == "debug") console.log("[DEBUG] Found Modules: ", jsFiles);
   return jsFiles;
 }
 
 async function loadModule(ModuleName) {
+  if (!modules.includes(ModuleName)) {
+    if (logging === "debug") console.log("[DEBUG] Cant find Module: ", ModuleName);
+    return null; // Modul nicht gefunden
+  }
+
   const imported = await import(path.join(modulesPath, ModuleName + ".js"));
   const moduleObj = imported.default || imported; // CommonJS kompatibel
-  if (logging === "debug") console.log("[DEBUG] Loaded Module: ", ModuleName);
+  if (logging === "debug") console.log("[DEBUG] Loaded new Module: ", ModuleName);
   modules_loaded.push(ModuleName);
   return moduleObj;
 }
 
-// Neue Funktion: alle Module laden
+async function runModule(module, command) {
+  const thisModule = await loadModule(module);
+  return await thisModule.commands.ˋ${command}ˋ.handler();
+}
+
 async function loadAllModules() {
   const moduleNames = await listModules();
   const loadedModules = {};
@@ -44,3 +60,6 @@ async function loadAllModules() {
 (async () => {
   const allModules = await loadAllModules();
 })();
+
+
+runModule("time", "getDate")
